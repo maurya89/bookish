5eda905c7552c36268610bff2ca541b034feca53
andy <andy@123789.org>
2017-09-22 12:21:08 +0530

    minor changes

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ export const connexionString = 'mongodb://localhost/bookish';
 export const baseApi = 'api';
 export const publicApi = 'public';
 export const JWTSECRET = 'AIzaSyACKSH6x58dHWw5wkVtgVZn0DrdxO8M08I';
-export const GOOGLE_BOOK_KEY = 'AIzaSyACKSH6x58dHWw5wkVtgVZn0DrdxO8M08I';
+export const GOOGLE_BOOK_KEY = 'AIzaSyAc73PEvd_a2OMCkSFKgL7sXAOi65Kp83g';
 export const mailConfig = {
     "service": 'gmail',
     "auth": {
